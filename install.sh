7cfe56aeddecb18f2248b4d6b6ce0d7c78484573
mamat159841 <110762686+mamat159841@users.noreply.github.com>
2023-05-09 21:58:27 +0800

    Update install.sh

--- install.sh
+++ install.sh
@@ -2,5 +2,9 @@
 
 yarn install
 npm i yt-search
+npm i -g pm2
+npm i pm2 -g
+yarn add pm2 -g
+yarn add -g pm2
 
 echo "All dependencies have been installed, please run the command \"npm start\" to immediately start the script"
