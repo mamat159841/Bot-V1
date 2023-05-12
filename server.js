86cd4d74bef9331c6b90973320821e2521e333e9
mamat159841 <110762686+mamat159841@users.noreply.github.com>
2023-05-12 08:51:30 +0800

    Update server.js

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 const express = require('express')
 const server = express()
-const PORT = process.env.PORT || 3000
+const PORT = process.env.PORT || 5000
 
 server.set('json spaces', 2)
 server.get('*', async (req, res) => {
